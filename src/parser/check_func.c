86925bff78124090aded6f6ed8f90d85f2448c58
Jae seok LEE <jaelee@student.42.fr>
2019-01-23 19:06:13 +0100

    new parser coming

---
+++ src/parser/check_func.c
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   check_func.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jaelee <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/01/23 17:13:50 by jaelee            #+#    #+#             */
+/*   Updated: 2019/01/23 17:24:13 by jaelee           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "lem-in.h"
+
+size_t  is_nbr_ants(char *line)
+{
+	    return (0);
+}
+
+size_t  is_start(char *line)
+{
+	    return (0);
+}
+
+size_t  is_end(char *line)
+{
+	    return (0);
+}
+
+size_t  is_command(char *line)
+{
+	    return (0);
+}
+
+size_t  is_comment(char *line)
+{
+	    return (0);
+}
+
+size_t  is_node(char *line)
+{
+	    return (0);
+}
+
+size_t is_edge(char *line)
+{
+	    return (0);
+}
