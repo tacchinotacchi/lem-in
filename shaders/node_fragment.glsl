94cde821c8d4b0bb46fd89f53b22c141d75b76bc
Alex AMADORI <aamadori@student.42.fr>
2019-02-06 22:40:14 +0100

    different texture for start and end node

--- shaders/node_fragment.glsl
+++ shaders/node_fragment.glsl
@@ -1,16 +1,20 @@
 #version 330
 
+in float texture_alphas[4];
 in vec2 texture_coords;
 
 layout(location = 0) out vec4 color;
 
-//uniform float start;
-//uniform float end;
 uniform sampler2D node_tex;
+uniform sampler2D start_tex;
+uniform sampler2D end_tex;
 
 void	main()
 {
-	color = texture(node_tex, texture_coords);
+	color = texture_alphas[0] * texture(node_tex, texture_coords);
+	color += texture_alphas[1] * texture(start_tex, texture_coords);
+	color += texture_alphas[2] * texture(end_tex, texture_coords);
+	color += texture_alphas[3] * texture(end_tex, texture_coords);
 	if (color.a < 0.9)
 		discard;
 }
