286202b6d7652ae6297c5cd9440f3f134cf81657
Jae Seok Lee <jaelee@student.42.fr>
2019-02-25 22:03:27 +0100

    added free_trees in lem_in.c when parsing fails, leak fixed

--- src/lem_in.c
+++ src/lem_in.c
@@ -6,7 +6,7 @@
 /*   By: jaelee <jaelee@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/27 21:30:13 by aamadori          #+#    #+#             */
-/*   Updated: 2019/02/25 18:42:40 by jaelee           ###   ########.fr       */
+/*   Updated: 2019/02/25 22:01:18 by jaelee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -54,6 +54,7 @@ int			main(void)
 	{
 		/*free_parser(&info);*/
 		free_lemin(&info);
+		free_trees(&info);
 		ft_dprintf(2, "ERROR\n");
 		return (0);
 	}
